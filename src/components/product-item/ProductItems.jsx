4135f1a01fb35fd96a9d96ecab20249e01140524
Muhammadyusufdeveloper <155054579+Muhammadyusufdevloper@users.noreply.github.com>
2024-07-25 11:38:10 +0500

    fil

--- src/components/product-item/ProductItems.jsx
+++ src/components/product-item/ProductItems.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
-
-const ProductItems = () => {
+import { Link } from "react-router-dom"
+import { GoHeart } from "react-icons/go"
+import "./productItem.scss"
+import { memo } from "react"
+import { FaRegStarHalfStroke } from "react-icons/fa6"
+import { FaRegStar, FaStar } from "react-icons/fa"
+const ProductItems = ({ data }) => {
+    const getRating = (rating) => {
+        let res = [];
+        for (let i = 0; i < Math.trunc(rating); i++) {
+            res.push(<FaStar />);
+        }
+        if (rating % 1 > 0.4) {
+            res.push(<FaRegStarHalfStroke />);
+        }
+        for (let i = Math.round(rating); i < 5; i++) {
+            res.push(<FaRegStar />);
+        }
+        return res;
+    };
     return (
-        <div>ProductItems</div>
+        <>
+            <div className="product-item__cards">
+                {
+                    data?.map((item) => (
+                        <div key={item.id} className="product-item__card">
+                            <div className="product-item__image-box">
+                                <div className="product-item__images-box__discount">
+                                    <p className="product-item__images-box__new-text">New</p>
+                                    <p className="product-item__images-box__discount-text">-50%</p>
+                                </div>
+                                <button className="product-item__image-box__heart"><GoHeart /></button>
+                                <Link to={`/single-routes/${item?.id}`}>
+                                    <img src={item?.images[0]} alt={item?.title} />
+                                </Link>
+                                <button className="product-item__image-box__add-to-cart">Add To Cart</button>
+                            </div>
+                            <div className="product-item__info-box">
+                                {getRating(item?.rating)}
+                                <h3 className="product-item__card-title">{item?.title}</h3>
+                                <div className="product-item__card-price-box">
+                                    <p className="product-item__price-text">${item?.price}</p>
+                                    <p className="product-item__price-old-text">${item?.oldPrice}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
     )
 }
 
-export default ProductItems
+export default memo(ProductItems)
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,14 +1,39 @@
-import { memo, useEffect } from "react"
+import { memo, useEffect, useState } from "react"
 import HeroBase from "../../components/hero/HeroBase"
 import Newsletter from "../../components/newsletter/Newsletter"
+import ProductItems from "../../components/product-item/ProductItems"
+import { useGetProductsQuery } from "../../context/api/productApi"
+import { Pagination } from "@mui/material"
+import { useGetCategoryQuery } from "../../context/api/categoryApi"
+import Category from "../../components/category/Category"
 
 const Shop = () => {
+    const [categoryValue, setCategoryValue] = useState("all")
+    const [page, setPage] = useState(1);
+    const category = categoryValue === "all" ? "" : categoryValue
+    const { data: lengthData } = useGetProductsQuery({ category })
+    const { data: product, isFetching, isLoading } = useGetProductsQuery({ category, limit: 9, page })
+    const { data: categoryList } = useGetCategoryQuery()
+
+    const handleChange = (event, value) => {
+        setPage(value);
+    };
+    let count = Math.floor(lengthData?.length / 9)
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [])
     return (
         <>
             <HeroBase title={"Shop Page"} desc={"Let’s design the place you always imagined."} path={"Shop"} isImages={false} />
+            <section>
+                <div className="min-container">
+                    <Category setPage={setPage} categoryValue={categoryValue} setCategoryValue={setCategoryValue} category={categoryList} />
+                    <ProductItems data={product} />
+                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "24px", marginBottom: "24px" }}>
+                        <Pagination count={count + 1} page={page} onChange={handleChange} />
+                    </div>
+                </div>
+            </section>
             <Newsletter />
         </>
     )
